e6ef82d95021dc7590b9274e3498867cf68ea02d
damian-castro10 <emilianocastro160905@gmail.com>
2026-03-16 15:54:21 -0300

    Agregue un ciclo for para mostrar Hola 5 veces

--- holaMundooo.java
+++ holaMundooo.java
@@ -3,5 +3,9 @@ public class holaMundooo{
         System.out.println("Mi primer commit!");
         System.out.println("estoy usando git de nuevo");
         
+        for(int i=0 ; i<5 ; i++)
+        {
+            System.out.println("Hola " + i+1);
+        }
     }
 }
